70b79a1b4f1523e6392f0f835e300da47005116d
karthickx1910 <karthickx1910@gmail.com>
2020-02-25 04:29:39 -0800

    SECOND COMMIT

--- SampleGIT/src/com/sample/App/SampleApp.java
+++ SampleGIT/src/com/sample/App/SampleApp.java
@@ -4,13 +4,10 @@ public class SampleApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		
-		
+	
 		System.out.println("Hello \t\t welcome");
 		
-		
-		
+	
 	}
 
 }
